18ee1924a877b3cdc2a00ba4af81bae806f44ade
Garnet Yeates <45212493+Garnet-Yeates@users.noreply.github.com>
2024-03-16 04:52:45 -0400

    Added GetAdjacentEmptyLocations (+1 overload) to EntityGrid.cs. Documentation as well

--- MonoGame WaTor/DataStructures/EntityGrid.cs
+++ MonoGame WaTor/DataStructures/EntityGrid.cs
@@ -52,6 +52,11 @@ namespace MonoGame_WaTor.DataStructures
             return GetAdjacentLocations(p.X, p.Y);
         }
 
+        /// <summary>
+        /// Conveniently enough, the default initialCapacity of a List is 4! So we can simply convert the result of this method to
+        /// a List using new List(GetAdjacentLocations(...)) (Enumerable constructor) without worrying about the List inefficiently 
+        /// re-allocating as a result of Count going past the Capacity, because we know GetAdjacentLocations returns exactly 4.
+        /// </summary>
         public IEnumerable<Point2D> GetAdjacentLocations(int x, int y)
         {
             if (x < NumEntitiesFitX - 1)
@@ -88,6 +93,22 @@ namespace MonoGame_WaTor.DataStructures
             else
             {
                 yield return new Point2D(x, NumEntitiesFitY - 1);
+            }
+        }
+
+        public IEnumerable<Point2D> GetAdjacentEmptyLocations(in Point2D p)
+        {
+            return GetAdjacentEmptyLocations(p.X, p.Y);
+        }
+
+        public IEnumerable<Point2D> GetAdjacentEmptyLocations(int x, int y)
+        {
+            foreach (var p in GetAdjacentLocations(x, y))
+            {
+                if (this[p] == null)
+                {
+                    yield return p;
+                }
             }
         }
     }
