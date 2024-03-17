4b2458e4d63250d4115015661e4ebdcf0c1551ec
Garnet Yeates <45212493+Garnet-Yeates@users.noreply.github.com>
2024-03-17 01:08:57 -0400

    Added Shark AI as well as related fields/properties

--- MonoGame WaTor/GameObjects/Shark.cs.cs
+++ MonoGame WaTor/GameObjects/Shark.cs.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame_WaTor.DataStructures;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MonoGame_WaTor.GameObjects
 {
     public class Shark : Entity
     {
+        public static int BaseFishTillBreed { get; set; }
+        private int fishTillBreed;
+
+        public static int EnergyGainedFromEatingFish { get; set; }
+        public static int BaseEnergy { get; set; }
+        private int energy;
+
         public static byte SharkGroupIndex => 0;
         public static Texture2D SharkTexture { get; private set; }
 
@@ -14,7 +24,11 @@ namespace MonoGame_WaTor.GameObjects
         public override Color Color => SharkColor;
         public override Texture2D Texture => SharkTexture;
 
-        public Shark(WaTorGame game, int x, int y) : base(game, x, y) { }
+        public Shark(WaTorGame game, int x, int y) : base(game, x, y)
+        {
+            energy = BaseEnergy;
+            fishTillBreed = BaseFishTillBreed;
+        }
 
         public static void LoadStaticContent(GraphicsDevice graphics)
         {
@@ -29,6 +43,50 @@ namespace MonoGame_WaTor.GameObjects
 
         public override void Update()
         {
+            energy--;
+
+            List<Point2D> fishSquares = new(), emptySquares = new();
+            foreach (var p in World.GetAdjacentLocations(X, Y))
+            {
+                if (World[p] is null)
+                {
+                    emptySquares.Add(p);
+                }
+                else if (World[p] is Fish)
+                {
+                    fishSquares.Add(p);
+                }
+            }
+
+            if (fishSquares.Any())
+            {
+                Point2D randomFishSquare = fishSquares[WaTorGame.R.Next(fishSquares.Count)];
+                Entity fishHere = World[randomFishSquare];
+                fishHere.RemoveFromWorld();
+
+                energy += EnergyGainedFromEatingFish;
+                fishTillBreed--;
+
+                if (fishTillBreed == 0)
+                {
+                    Fish child = new(Game, randomFishSquare.X, randomFishSquare.Y);
+                    child.AddToWorld(updateOnCurrentUpdate: false);
+                }
+                else
+                {
+                    Move(randomFishSquare);
+                }
+                // Energy inc, possibly breed
+            }
+            else if (emptySquares.Any())
+            {
+                Move(emptySquares[WaTorGame.R.Next(fishSquares.Count)]);
+            }
+
+            if (energy == 0)
+            {
+                RemoveFromWorld();
+            }
         }
     }
 }
